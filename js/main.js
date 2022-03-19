802497d43fafdb0746530ec9ee7e8ada4c5a0441
TP-Interprice <temuriy1998@gmail.com>
2022-03-20 02:13:40 +0500

    Calculator js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let numbers = document.querySelectorAll('.num')
+let sign = document.querySelectorAll('.sign')
+let display = document.querySelector('input')
+let dot = document.querySelector('.dot')
+let equal = document.querySelector('.equal')
+let clearElement = document.querySelectorAll('remove-element')[0]
+let removeElement = document.querySelectorAll('remove-element')[1]
+console.log(clearElement);
